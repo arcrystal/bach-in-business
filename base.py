4dba4aa5cd4f00c11075d59ab01da485265a4a7a
king-brendan <31732221+king-brendan@users.noreply.github.com>
2023-04-06 15:45:48 -0400

    added CORS

--- base.py
+++ base.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 import json
 
 api = Flask(__name__)
+CORS(api)
 
 @api.route('/get_music')
 def my_get_music():
-    with open('sampleJSON.json', 'r') as f:
-        sample = json.loads(f.read())
-    response_body = sample
+    #with open('fug_sin.xml', 'r') as f:
+        #sample = json.loads(f.read())
+    #response_body = sample
     # fill this in with returning midi file
-
-    return response_body
+    return {
+        "filepath": "Filepath of generated file here"
+    }
