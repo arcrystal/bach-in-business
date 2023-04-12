13a7dc26893643963258ff5b02b02f37f2ca7db3
arcrystal <acrystal@u.rochester.edu>
2023-04-11 21:30:50 -0400

    updates

--- pipeline.py
+++ pipeline.py
@@ -11,7 +11,6 @@ import argparse
 import contextlib
 from fractions import Fraction
 import os
-import re
 with contextlib.redirect_stdout(None):
     import pygame
 
@@ -28,9 +27,9 @@ def flatten_midi(file):
 
     return parts.flatten()
 
-def feature_extraction(folders, flatten=False):
+def extract(folders, flatten=True):
     all_notes = []
-    all_durations = []
+    all_durs = []
     for folder in folders:
         if '.mid' in folder:
             files = ["Music/"+ folder]
@@ -38,7 +37,7 @@ def feature_extraction(folders, flatten=False):
             files = glob.glob("Music/"+ folder + "/*.mid")
         for file in files:
             notes = []
-            durations = []
+            durs = []
             print("Extracting:", file)
             if flatten:
                 notes_to_parse = flatten_midi(file)
@@ -55,78 +54,79 @@ def feature_extraction(folders, flatten=False):
             for element in notes_to_parse:
                 if isinstance(element, note.Note):
                     notes.append(str(element.pitch))
-                    durations.append(element.duration.quarterLength)
+                    durs.append(element.duration.quarterLength)
                 elif isinstance(element, chord.Chord):
                     notes.append(".".join(str(n) for n in element.normalOrder))
-                    durations.append(element.duration.quarterLength)
+                    durs.append(element.duration.quarterLength)
 
             all_notes.append(notes)
-            all_durations.append(durations)
+            all_durs.append(durs)
     
-    return all_notes, all_durations
+    return all_notes, all_durs
 
-def preprocess_data(all_notes, all_durations, lookback=100):
+def transform(all_notes, all_durs, lookback=100):
     pitchnames = sorted(set(item for notes in all_notes for item in notes))
     note_to_int = {note:number for number, note in enumerate(pitchnames)}
-    duration_names = sorted(set(item for durations in all_durations for item in durations))
-    duration_to_int = {dur:number for number, dur in enumerate(duration_names)}
+    dur_names = sorted(set(item for durs in all_durs for item in durs))
+    dur_to_int = {dur:number for number, dur in enumerate(dur_names)}
 
     in_notes = []
-    in_durations = []
+    in_durs = []
     out_notes = []
-    out_durations = []
-    for notes, durations in zip(all_notes, all_durations):
+    out_durs = []
+    for notes, durs in zip(all_notes, all_durs):
         for i in range(len(notes) - lookback):
             sequence_in_notes = notes[i:i + lookback]
-            sequence_in_durations = durations[i:i + lookback]
+            sequence_in_durs = durs[i:i + lookback]
             sequence_out_notes = notes[i + lookback]
-            sequence_out_durations = durations[i + lookback]
+            sequence_out_durs = durs[i + lookback]
 
             in_notes.append([note_to_int[char] for char in sequence_in_notes])
-            in_durations.append([duration_to_int[char] for char in sequence_in_durations])
+            in_durs.append([dur_to_int[char] for char in sequence_in_durs])
             out_notes.append(note_to_int[sequence_out_notes])
-            out_durations.append(duration_to_int[sequence_out_durations])
+            out_durs.append(dur_to_int[sequence_out_durs])
 
     in_notes = np.reshape(in_notes, (len(in_notes), lookback, 1))
-    in_durations = np.reshape(in_durations, (len(in_durations), lookback, 1))
+    in_durs = np.reshape(in_durs, (len(in_durs), lookback, 1))
 
     out_notes = to_categorical(out_notes)
-    out_durations = to_categorical(out_durations)
+    out_durs = to_categorical(out_durs)
 
-    return in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names
+    return in_notes, in_durs, out_notes, out_durs, pitchnames, dur_names
 
-def build_model(lookback, output_size_notes, output_size_durations, n_units=512):
+def build_model(lookback, output_size_notes, output_size_durs, n_units=512):
 
     in_notes = Input(shape=(lookback, 1))  # Adjusted input shape for notes
-    in_durations = Input(shape=(lookback, 1))  # Adjusted input shape for durations
+    in_durs = Input(shape=(lookback, 1))  # Adjusted input shape for durs
     
     # Process notes
     lstm_notes_1 = LSTM(n_units, return_sequences=True)(in_notes)
     dropout_notes_1 = Dropout(0.3)(lstm_notes_1)
     lstm_notes_2 = LSTM(n_units)(dropout_notes_1)
     
-    # Process durations
-    lstm_durations_1 = LSTM(n_units, return_sequences=True)(in_durations)
-    dropout_durations_1 = Dropout(0.3)(lstm_durations_1)
-    lstm_durations_2 = LSTM(n_units)(dropout_durations_1)
+    # Process durs
+    lstm_durs_1 = LSTM(n_units, return_sequences=True)(in_durs)
+    dropout_durs_1 = Dropout(0.3)(lstm_durs_1)
+    lstm_durs_2 = LSTM(n_units)(dropout_durs_1)
     
     # Fusion layer
-    fusion = Concatenate()([lstm_notes_2, lstm_durations_2])
+    fusion = Concatenate()([lstm_notes_2, lstm_durs_2])
     dense_fusion = Dense(n_units)(fusion)
     dropout_fusion = Dropout(0.3)(dense_fusion)
 
-    # Output layers for notes and durations
+    # Output layers for notes and durs
     out_notes = Dense(output_size_notes, activation="softmax", name="notes")(dropout_fusion)
-    out_durations = Dense(output_size_durations, activation="softmax", name="durations")(dropout_fusion)
+    out_durs = Dense(output_size_durs, activation="softmax", name="durs")(dropout_fusion)
     
-    model = Model(inputs=[in_notes, in_durations], outputs=[out_notes, out_durations])
+    model = Model(inputs=[in_notes, in_durs], outputs=[out_notes, out_durs])
     model.compile(loss=["categorical_crossentropy", "categorical_crossentropy"],
                   optimizer='adam', # 'rmsprop',
                   metrics=["accuracy"])
     print(model.summary())
     return model
 
-def train_model(model, in_notes, in_durations, out_notes, out_durations, epochs, batch_size, fname='model', verbose=2):
+def train_model(model, in_notes, in_durs, out_notes, out_durs,
+                epochs, batch_size, fname='model', verbose=2):
     my_callbacks = [
         ModelCheckpoint(filepath=f'Models/{fname}.h5',
                         save_best_only=True,
@@ -134,7 +134,7 @@ def train_model(model, in_notes, in_durations, out_notes, out_durations, epochs,
                         mode='min'),
         TensorBoard(log_dir=f'./Logs/{fname}/')
     ]
-    history = model.fit([in_notes, in_durations], [out_notes, out_durations],
+    history = model.fit([in_notes, in_durs], [out_notes, out_durs],
                         epochs=epochs, batch_size=batch_size, callbacks=my_callbacks,
                         verbose=verbose)
     return history
@@ -142,15 +142,15 @@ def train_model(model, in_notes, in_durations, out_notes, out_durations, epochs,
 def plot_history(history, plot_fname='training_curves'):
     fig, ax = plt.subplots(1, 2, figsize=(10,5))
     ax[0].plot(history.history['notes_accuracy'])
-    ax[0].plot(history.history['durations_accuracy'])
+    ax[0].plot(history.history['durs_accuracy'])
     ax[0].set_title('Accuracy Curves')
     ax[0].set_xlabel('epoch')
-    ax[0].legend(['notes_accuracy', 'durations_accuracy'], loc='lower right')
+    ax[0].legend(['notes_accuracy', 'durs_accuracy'], loc='lower right')
     ax[1].plot(history.history['notes_loss'])
-    ax[1].plot(history.history['durations_loss'])
+    ax[1].plot(history.history['durs_loss'])
     ax[1].set_title('Loss Curves')
     ax[1].set_xlabel('epoch')
-    ax[1].legend(['notes_loss', 'durations_loss'], loc='upper right')
+    ax[1].legend(['notes_loss', 'durs_loss'], loc='upper right')
     plt.savefig("Figures/" + plot_fname + ".png")
     plt.show()
 
@@ -161,13 +161,13 @@ def load_music_model(fname='model'):
         print(f'Error: could not load "Models/{fname}.h5')
         exit()
     
-def generate_music(model, network_input_notes, network_input_durations, pitchnames, duration_names, num_notes):
-    start = np.random.randint(0, len(network_input_notes) - 1)
+def generate_music(model, in_notes, in_durs, pitchnames, dur_names, num_notes):
+    start = np.random.randint(0, len(in_notes) - 1)
     int_to_note = {number:note for number, note in enumerate(pitchnames)}
-    int_to_duration = {number:dur for number, dur in enumerate(duration_names)}
+    int_to_dur = {number:dur for number, dur in enumerate(dur_names)}
 
-    pattern_notes = network_input_notes[start]
-    pattern_durs = network_input_durations[start]
+    pattern_notes = in_notes[start]
+    pattern_durs = in_durs[start]
     prediction_output = []
 
     for _ in range(num_notes):
@@ -178,7 +178,7 @@ def generate_music(model, network_input_notes, network_input_durations, pitchnam
         index_notes = np.argmax(note_pred)
         index_durs = np.argmax(dur_pred)
         note_result = int_to_note[index_notes]
-        dur_result = int_to_duration[index_durs]
+        dur_result = int_to_dur[index_durs]
         prediction_output.append((note_result, dur_result))
 
         pattern_notes = np.append(pattern_notes, index_notes)
@@ -239,13 +239,13 @@ def play_music(fname):
         while pygame.mixer.music.get_busy():
             clock.tick(30) # check if playback has finished
 
-def save_data(fname, in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names):
+def save_data(fname, in_notes, in_durs, out_notes, out_durs, pitchnames, dur_names):
     if not os.path.exists(f'Data/{fname}'):
         os.mkdir(f'Data/{fname}')
     np.save(f'Data/{fname}/in_notes.npy', in_notes)
-    np.save(f'Data/{fname}/in_durations.npy', in_durations)
+    np.save(f'Data/{fname}/in_durs.npy', in_durs)
     np.save(f'Data/{fname}/out_notes.npy', out_notes)
-    np.save(f'Data/{fname}/out_durations.npy', out_durations)
+    np.save(f'Data/{fname}/out_durs.npy', out_durs)
 
     with open(f'Data/{fname}/pitchnames', 'w') as f:
         s = ""
@@ -254,47 +254,48 @@ def save_data(fname, in_notes, in_durations, out_notes, out_durations, pitchname
 
         f.write(s[:-1])
 
-    with open(f'Data/{fname}/duration_names', 'w') as f:
+    with open(f'Data/{fname}/dur_names', 'w') as f:
         s = ""
-        for d in duration_names:
+        for d in dur_names:
             s += str(d) + " "
 
         f.write(s[:-1])
 
 def load_data(fname):
     in_notes = np.load(f'Data/{fname}/in_notes.npy')
-    in_durations = np.load(f'Data/{fname}/in_durations.npy')
+    in_durs = np.load(f'Data/{fname}/in_durs.npy')
     out_notes = np.load(f'Data/{fname}/out_notes.npy')
-    out_durations = np.load(f'Data/{fname}/out_durations.npy')
+    out_durs = np.load(f'Data/{fname}/out_durs.npy')
 
     with open(f'Data/{fname}/pitchnames', 'r') as f:
         pitchnames = [x for x in f.read().split()]
 
-    with open(f'Data/{fname}/duration_names', 'r') as f:
-        duration_names = []
+    with open(f'Data/{fname}/dur_names', 'r') as f:
+        dur_names = []
         for x in f.read().split():
             if '.' in x:
-                duration_names.append(float(x))
+                dur_names.append(float(x))
             elif '/' in x:
-                duration_names.append(Fraction(x))
+                dur_names.append(Fraction(x))
             else:
-                print("Error: duration_name string is unreadable:", x)
+                print("Error: dur_name string is unreadable:", x)
                 exit()
 
-    return in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names
+    return in_notes, in_durs, out_notes, out_durs, pitchnames, dur_names
 
 def parse_args():
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('-m',  '--Music',     nargs = '*', help='input mid foldername(s)')
-    argparser.add_argument('-t',  '--Train',     action='store_true', help='input whether to train model')
-    argparser.add_argument('-n',  '--Name',      help='input name for model, figure, generated music')
-    argparser.add_argument('-e',  '--Epochs',    help='input name for model, figure, generated music')
+    argparser.add_argument('-m',  '--Music',     nargs='*', help='input .mid foldername or pattern')
+    argparser.add_argument('-t',  '--Train',     action='store_true', help='Whether to train a model')
+    argparser.add_argument('-n',  '--Name',      help='Name for model, figure, and generated music')
+    argparser.add_argument('-e',  '--Epochs',    help='Number of training epochs')
     argparser.add_argument('-s',  '--SaveData',  action='store_true', help='Save preprocessed data')
     argparser.add_argument('-l',  '--LoadData',  action='store_true', help='Load preprocessed data')
     argparser.add_argument('-u',  '--Use',       help='Use a specific model')
     argparser.add_argument('-b',  '--BatchSize', help='Size of training batches')
-    argparser.add_argument('-v',  '--Verbose',   help='Size of training batches')
-    argparser.add_argument('-nn', '--NumNotes',   help='Num notes generated')
+    argparser.add_argument('-v',  '--Verbose',   help='Tensorflow model fitting parameter')
+    argparser.add_argument('-nn', '--NumNotes',  help='Number of notes generated')
+    argparser.add_argument('-lb', '--Lookback',  help='Lookback of model architecture')
     
     args = argparser.parse_args()
     if not args.Music:
@@ -311,52 +312,75 @@ def parse_args():
             fname = ".".join(args.Music).replace('/','_')
 
     if args.Music:
-        print(f'\t--Music     : {", ".join(args.Music)}')
+        print(f'\t--Music      : {", ".join(args.Music)}')
     else:
-        print(f'\t--Music     : {fname}')
+        print(f'\t--Music      : {fname}')
 
-    print(f'\t--Train     : {args.Train}')
-    print(f'\t--Name      : {args.Name}')
-    print(f'\t--Epochs    : {args.Epochs}')
-    print(f'\t--SaveData  : {args.SaveData}')
-    print(f'\t--LoadData  : {args.LoadData}')
-    print(f'\t--Use       : {args.Use}')
-    print(f'\t--BatchSize : {args.BatchSize}')
-    print(f'\t--NumNotes  : {args.NumNotes}')
-    print(f'\t--Verbose   : {args.Verbose}')
+    print(f'\t--Train      : {args.Train}')
+    print(f'\t--Name       : {args.Name}')
+    print(f'\t--Epochs     : {args.Epochs}')
+    print(f'\t--SaveData   : {args.SaveData}')
+    print(f'\t--LoadData   : {args.LoadData}')
+    print(f'\t--Use        : {args.Use}')
+    print(f'\t--BatchSize  : {args.BatchSize}')
+    print(f'\t--NumNotes   : {args.NumNotes}')
+    print(f'\t--Verbose    : {args.Verbose}')
+    print(f'\t--Lookback   : {args.Lookback}')
     print()
     return args, fname
 
 def music_generation_pipeline(lookback=512, epochs=75, batch_size=64, num_notes=200, verbose=1):
     print()
     args, fname = parse_args()
+    if args.Lookback:
+        try:
+            lookback = int(args.Lookback)
+        except ValueError:
+            pass
     if args.LoadData:
-        in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names = load_data(fname)
+        in_notes, in_durs, out_notes, out_durs, pitch, durs = load_data(fname)
     else:
-        all_notes, all_durations = feature_extraction(args.Music)
+        all_notes, all_durs = extract(args.Music)
         print("\nTransforming data...")
-        in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names = preprocess_data(all_notes, all_durations, lookback)
+        in_notes, in_durs, out_notes, out_durs, pitch, durs = transform(all_notes, all_durs, lookback)
     
     if args.SaveData:
-        save_data(fname, in_notes, in_durations, out_notes, out_durations, pitchnames, duration_names)
+        save_data(fname, in_notes, in_durs, out_notes, out_durs, pitch, durs)
 
     print("\nLoading data. Shape:", in_notes.shape)
-    if args.NumNotes.isdigit():
-        num_notes = int(args.NumNotes)
+    if args.NumNotes:
+        try:
+            num_notes = int(args.NumNotes)
+        except ValueError:
+            pass
 
     if args.Train:
-        if args.Lookback.isdigit():
-            lookback = int(args.Lookback)
-        if args.Epochs.isdigit():
-            epochs = int(args.Epochs)
-        if args.BatchSize.isdigit():
-            batch_size = int(args.BatchSize)
-        if args.Verbose.isdigit():
-            verbose = int(args.Verbose)
+        if args.Epochs:
+            try:
+                epochs = int(args.Epochs)
+            except ValueError:
+                pass
+            
+        if args.BatchSize:
+            try:
+                batch_size = int(args.BatchSize)
+            except ValueError:
+                pass
+            
+        if args.Verbose:
+            try:
+                verbose = int(args.Verbose)
+            except ValueError:
+                pass
 
         print()
-        model = build_model(lookback, out_notes.shape[1], out_durations.shape[1])
-        history = train_model(model, in_notes, in_durations, out_notes, out_durations, epochs, batch_size, fname, verbose=verbose)
+        if args.Use:
+            model = load_music_model(args.Use)
+        else:
+            model = build_model(lookback, out_notes.shape[1], out_durs.shape[1])
+
+        history = train_model(model, in_notes, in_durs, out_notes, out_durs,
+                              epochs, batch_size, fname, verbose)
         plot_history(history, fname)
         print("\nFinished training.")
     if args.Use:
@@ -364,16 +388,16 @@ def music_generation_pipeline(lookback=512, epochs=75, batch_size=64, num_notes=
     else:
         model = load_music_model(fname)
     print("\nGenerating music.")
-    generated_music = generate_music(model, in_notes, in_durations, pitchnames, duration_names, num_notes)
+    generated_music = generate_music(model, in_notes, in_durs, pitch, durs, num_notes)
     f = create_midi(generated_music, fname)
     print(f"Saved midi file at: '{f}'")
     print("\nPlaying music.")
     play_music(f)
 
 def get_music_filename(fname, num_notes):
-    in_notes, in_durations, _, _, pitchnames, duration_names = load_data(fname)
+    in_notes, in_durs, _, _, pitch, durs = load_data(fname)
     model = load_music_model(fname)
-    generated_music = generate_music(model, in_notes, in_durations, pitchnames, duration_names, num_notes)
+    generated_music = generate_music(model, in_notes, in_durs, pitch, durs, num_notes)
     midi = create_midi(generated_music, fname)
     print(midi)
     play_music(midi)
